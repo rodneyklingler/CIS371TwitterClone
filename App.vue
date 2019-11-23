e8d3208ac6c349acb30a4453d6a16e2feda281c7
RowanBixler <57460321+RowanBixler@users.noreply.github.com>
2019-11-23 14:46:30 -0500

    Add files via upload

--- App.vue
+++ App.vue
@@ -2,28 +2,10 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      id="toolbar"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <span>TWITTER CLONE</span>
 
       <v-spacer></v-spacer>
       <v-btn @click="ButtonHandler">SignOut</v-btn>
@@ -34,6 +16,16 @@
     </v-content>
   </v-app>
 </template>
+
+<style>
+  #toolbar {
+    background-color: rgba(119, 181, 215,0.5);
+    /* Experimental web feature (Can be viewed in Microsoft Edge) */
+    backdrop-filter: saturate(180%) blur(20px);  
+    -webkit-backdrop-filter: saturate(180%) blur(20px);
+    position: fixed;
+  }
+</style>
 
 <script>
 import Login from '../src/Login.vue';
